e7995f276c9e0325517a2fc9a56aad603621aaca
RUMENRUSCHEV <61242729+RUMENRUSCHEV@users.noreply.github.com>
2020-03-25 14:59:49 +0200

    First Commit

---
+++ Homework2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace Homework2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string[] input = new string[text.Count()];
+            int f = 0;
+            foreach (var item in text)
+            {
+                input[f] = item.ToString();
+                f++;
+            }
+
+            string word = "";
+            for (int i = 3; i < text.Count(); i++)
+            {
+                word += input[i];
+            }
+
+            string word2 = "";
+            for (int i = 0; i < text.Count() - 3; i++)
+            {
+                word2 += input[i];
+            }
+
+            int n = int.Parse(Console.ReadLine());
+            string word3 = "";
+
+            for (int i = n; i < text.Count() - 3; i++)
+            {
+                word3 += input[i];
+            }
+
+            Console.WriteLine(word);
+            Console.WriteLine(word2);
+            Console.WriteLine(word3);
+            Console.Read();
+        }
+    }
+}
